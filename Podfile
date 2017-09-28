5b1529c806920c96cedf1ea21d030b40b9b70778
Unknown <itomclements@googlemail.com>
2017-09-28 23:31:23 +0100

    Added firebase auth

---
+++ Podfile
@@ -0,0 +1,26 @@
+project 'Project Athena.xcodeproj'
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Project Athena' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Project Athena
+  
+  
+pod 'Firebase/Core'
+pod 'Firebase/Auth'
+
+  target 'Project AthenaTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Project AthenaUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ Project Athena/AuthViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  Project Athena
+//
+//  Created by Thomas Clements on 27/09/2017.
+//  Copyright © 2017 Thomas Clements. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class AuthViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var topButton: UIButton!
+    
+    @IBOutlet weak var bottomButton: UIButton!
+    
+    var loginMode = true
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+    
+    
+    @IBAction func topTapped(_ sender: Any) {
+        
+        if let email = emailTextField.text{
+            if let password = passwordTextField.text{
+                if loginMode{
+                    // Login
+                } else {
+                    // Sign up
+                    Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if let error = error {
+                            print(error)
+                            
+                        }else {
+                            print("sign up success")
+                        }
+                    })
+                }
+            }
+        }
+        
+    }
+    
+    @IBAction func bottomTapped(_ sender: Any) {
+        if loginMode {
+            //Switch to sign up
+            topButton.setTitle("Register", for: .normal)
+            bottomButton.setTitle("Switch to Login", for: .normal)
+            loginMode = false
+        } else {
+            //Switch to login
+            topButton.setTitle("Login", for: .normal)
+            bottomButton.setTitle("Switch to Sign Up", for: .normal)
+            loginMode = true
+        }
+    }
+}
+
